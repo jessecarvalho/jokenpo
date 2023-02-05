ca166bd710e277debcfc07ddd65c28522770e00a
Jessé Carvalho <60759322+jessecarvalho@users.noreply.github.com>
2023-02-05 15:37:10 -0300

    fixture 4

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import ConfigurationView from "../views/ConfigurationView.vue";
+import GameView from "../views/GameView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,25 +14,22 @@ const router = createRouter({
     {
       path: "/configuration",
       name: "configuration",
-      component: () => import("../views/ConfigurationView.vue"),
+      component: ConfigurationView,
     },
     {
       path: "/game",
       name: "game",
-      component: () => import("../views/GameView.vue"),
+      component: GameView,
     },
   ],
 });
 
-// router.beforeEach((to, from, next) => {
-//   console.log(to);
-//   console.log(from);
-//   console.log(next);
-//   // if (to.name !== "home" && !from.name) {
-//   //   // next({ name: "home" });
-//   // } else {
-//   //   next();
-//   // }
-// });
+router.beforeEach((to, from, next) => {
+  if (to.name !== "home" && !from.name) {
+    next({ name: "home" });
+  } else {
+    next();
+  }
+});
 
 export default router;
